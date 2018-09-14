392775615c35943282498cf399a0fa184b8da781
Justin Golden <justingolden21@gmail.com>
2018-09-13 17:35:57 -0700

    added roll animation, code cleaning

--- roller-scripts.js
+++ roller-scripts.js
@@ -1,13 +1,32 @@
+//roll on button click, page load and enter key
 $(document).ready(function() {
-	$('#rollButton').on('click', doRoll).click();
+	$('#rollButton').on('click', roll).click();
 });
+document.onkeyup = function(e) {
+  let code = e.keyCode ? e.keyCode : e.which;
+  if(code == 13) roll(); //enter
+}
 
-const targetImgs = ['target1', 'target2', 'target3', 'target4', 'target forge', 'target forge'];
-const damageImgs = ['small flame', 'med flame', 'large flame'];
+//names for images and for description
+const targetImgs = ['slot 1', 'slot 2', 'slot 3', 'slot 4', 'forge', 'forge'];
+const damageImgs = ['small explosion', 'medium explosion', 'large explosion'];
+let rolls = [];
+
+function roll() {
+	let intvl = setInterval(doRoll, 100);
+	setTimeout(function() {
+		clearInterval(intvl);
+
+		doRoll();
+		$('#desc').html('Hit ' + targetImgs[rolls[0] ] + ' with a ' + damageImgs[rolls[1] ] );
+		$('#numRolls').html(parseInt($('#numRolls').html() )+1);
+	}, 500);
+}
 
 function doRoll() {
-	$('#targetDie').attr('src', 'assets/dice/' + targetImgs[getRandom(6)] + '.svg');
-	$('#damageDie').attr('src', 'assets/dice/' + damageImgs[getRandom(3)] + '.svg');
+	rolls = [getRandom(6), getRandom(3)];
+	$('#targetDie').attr('src', 'assets/dice/' + targetImgs[rolls[0] ] + '.svg');
+	$('#damageDie').attr('src', 'assets/dice/' + damageImgs[rolls[1] ] + '.svg');
 }
 
 function getRandom(sides) {
