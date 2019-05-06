15d851c1a194c0f3e8589e11832806edbbc55f53
Justin Golden <justingolden21@gmail.com>
2019-05-06 04:01:16 -0700

    add energy tracker

--- assets/js/roller-scripts.js
+++ assets/js/roller-scripts.js
@@ -1,10 +1,31 @@
 //roll on button click, page load and enter key
 $(document).ready(function() {
 	$('#rollButton').on('click', roll).click();
+	// energy
+	$('#plusButton').click(function() {
+		$('#energyModal .modal-body').append('<hr><div class="playerEnergy">'+
+			'<img class="energyCounter" onclick="toggleEnergy($(this) )" src="../assets/images/game-icons/icon-energy.svg">'+
+			'<img class="energyCounter" onclick="toggleEnergy($(this) )" src="../assets/images/game-icons/icon-energy.svg">'+
+			'<img class="energyCounter" onclick="toggleEnergy($(this) )" src="../assets/images/game-icons/icon-energy.svg">'+
+			'<img class="energyCounter" onclick="toggleEnergy($(this) )" src="../assets/images/game-icons/icon-energy.svg">'+
+        '</div>');
+	}).click().click();
+	$('#minusButton').click(function() {
+		$('#energyModal .modal-body .playerEnergy').last().remove();
+		$('#energyModal .modal-body hr').last().remove();
+	});
 });
 document.onkeyup = function(e) {
   let code = e.keyCode ? e.keyCode : e.which;
   if(code == 13 && ! $('#rollButton').is(':focus') ) roll(); //enter
+}
+
+function toggleEnergy(elm) {
+	if(elm.prop('src').includes('-empty') ) {
+		elm.prop('src', elm.prop('src').replace('icon-energy-empty', 'icon-energy') );
+	} else {
+		elm.prop('src', elm.prop('src').replace('icon-energy', 'icon-energy-empty') );			
+	}
 }
 
 //names for images and for description
