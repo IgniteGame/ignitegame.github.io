fa271f5ab8377f59c9de5c8326f806239c494339
RGB Studios <justingolden21@gmail.com>
2019-05-06 21:39:28 -0700

    added hand, active cards

--- assets/js/trial-scripts.js
+++ assets/js/trial-scripts.js
@@ -1,11 +1,20 @@
 $(function() {
   $('.energy').click(function() {
-    if($(this).prop('src').includes('-empty') ) {
+    if($(this).prop('src').includes('-empty') )
       $(this).prop('src', $(this).prop('src').replace('icon-energy-empty', 'icon-energy') );
-    } else {
-      $(this).prop('src', $(this).prop('src').replace('icon-energy', 'icon-energy-empty') );      
-    }
+    else
+      $(this).prop('src', $(this).prop('src').replace('icon-energy', 'icon-energy-empty') );
   });
+
+  $('#toggleHandBtn').click(toggleHand);
+
+  $('.gamecard').click(function() {
+    if(! $(this).hasClass('active') )
+      $('.gamecard').removeClass('active');
+    $(this).toggleClass('active');
+
+  });
+
 });
 
 // slot is slot1, slot2, slot3, slot4, forge, scrap
@@ -14,3 +23,16 @@ function updateCard(slot, card) {
   $('#' + slot).find('img:first').prop('src', '/assets/images/cards/' + card + '.png');
 }
 
+function toggleHand() {
+  if($('#hand').css('visibility') != 'hidden')
+    $('#hand').css('visibility', 'hidden');
+  else
+    $('#hand').css('visibility', '');
+}
+
+function addCardToHand(card) {
+  $('#hand').append('<img src="/assets/images/cards/' + card + '.png" class="gamecard">');
+}
+function removeCardFromHand(elm) {
+  elm.remove();
+}
