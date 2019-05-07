10402b5294456c658a5c3646e128239d9482ff66
RGB Studios <justingolden21@gmail.com>
2019-05-06 21:49:03 -0700

    you can now mvoe cards from hand to lineup

--- assets/js/trial-scripts.js
+++ assets/js/trial-scripts.js
@@ -1,4 +1,5 @@
 $(function() {
+  // toggle energy on click
   $('.energy').click(function() {
     if($(this).prop('src').includes('-empty') )
       $(this).prop('src', $(this).prop('src').replace('icon-energy-empty', 'icon-energy') );
@@ -6,14 +7,26 @@ $(function() {
       $(this).prop('src', $(this).prop('src').replace('icon-energy', 'icon-energy-empty') );
   });
 
+  // toggle hand on btn click
   $('#toggleHandBtn').click(toggleHand);
 
+  $('.lineup.slot .gamecard').click(function() {
+    
+    $(this).parent().html($('.active') );
+    // let slotNum = $(this).parent().attr('id')[4];
+    // console.log(slotNum);
+    
+  });
+
+  // clicking on card toggles its active class, removes other actives if it's active
   $('.gamecard').click(function() {
     if(! $(this).hasClass('active') )
       $('.gamecard').removeClass('active');
     $(this).toggleClass('active');
-
   });
+
+
+
 
 });
 
@@ -23,6 +36,7 @@ function updateCard(slot, card) {
   $('#' + slot).find('img:first').prop('src', '/assets/images/cards/' + card + '.png');
 }
 
+// toggle visability of hand
 function toggleHand() {
   if($('#hand').css('visibility') != 'hidden')
     $('#hand').css('visibility', 'hidden');
@@ -30,9 +44,11 @@ function toggleHand() {
     $('#hand').css('visibility', '');
 }
 
+// add card number to hand
 function addCardToHand(card) {
   $('#hand').append('<img src="/assets/images/cards/' + card + '.png" class="gamecard">');
 }
+// remove DOM elm from hand
 function removeCardFromHand(elm) {
   elm.remove();
 }
